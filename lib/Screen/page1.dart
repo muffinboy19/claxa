a257b5ffbb04ca4d9035d81e7d878963db6f2adc
muffinboy <87519281+muffinboy19@users.noreply.github.com>
2024-05-26 02:44:31 +0530

    modifed the README.md file

--- lib/Screen/page1.dart
+++ lib/Screen/page1.dart
@@ -129,9 +129,9 @@ class Page1 extends StatelessWidget {
           boxShadow: [
             BoxShadow(
               color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 5,
+              spreadRadius: 2,
               blurRadius: 7,
-              offset: const Offset(0, 3), // changes position of shadow
+              offset: const Offset(0, 4), // changes position of shadow
             ),
           ],
         ),
